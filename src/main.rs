fbefb481122514fb9006c7c4df7fbf0365d34309
Nicholas Ricciuti <rishooty@gmail.com>
2023-08-31 18:58:02 -0400

    initial drafts up to step 8

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+use minifb::{Key, Window, WindowOptions};
+use std::time::{Duration, Instant};
+use libloading::Library;
+use libc::c_void;
+
+pub type EnvironmentCallback = unsafe extern "C" fn(command: libc::c_uint, data: *mut libc::c_void) -> bool;
+
+unsafe extern "C" fn libretro_environment_callback(command: u32, data: *mut c_void) -> bool {
+    println!("libretro_environment_callbac Called with command: {}", command);
+    false
+}
+const EXPECTED_LIB_RETRO_VERSION: u32 = 1;
+
+fn load_core() {
+    unsafe {
+        let core = Library::new("gambatte_libretro.dylib").expect("Failed to load Core");
+        let retro_init: unsafe extern "C" fn() = *(core.get(b"retro_init").unwrap());
+        let retro_api_version: unsafe extern "C" fn() -> libc::c_uint = *(core.get(b"retro_api_version").unwrap());
+        let retro_set_environment: unsafe extern "C" fn(callback: EnvironmentCallback) = *(core.get(b"retro_set_environment").unwrap());
+        let api_version = retro_api_version();
+        println!("API Version: {}", api_version);
+        if api_version != EXPECTED_LIB_RETRO_VERSION {
+            panic!("The Core has been compiled with a LibRetro API that is unexpected, we expected version to be: {} but it was: {}", EXPECTED_LIB_RETRO_VERSION, api_version)
+        }
+        retro_set_environment(libretro_environment_callback);
+        retro_init();
+    }
+}
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = 480;
+
+fn main() {
+    load_core();
+
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut window = Window::new("Rust Game", WIDTH, HEIGHT, WindowOptions::default())
+        .unwrap_or_else(|e| {
+            panic!("{}", e);
+        });
+
+    //window.limit_update_rate(Some(std::time::Duration::from_micros(16600))); // ~60fps
+
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+
+    let mut fps_timer = Instant::now();
+    let mut fps_counter: i32 = 0;
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        // Clear the previous pixel to black
+        buffer[y * WIDTH + x] = 0x00000000;
+        fps_counter += 1;
+        let elapsed = fps_timer.elapsed();
+        if elapsed >= Duration::from_secs(1) {
+            let fps = fps_counter as f64 / elapsed.as_secs_f64();
+            window.set_title(&format!("Rust Game (FPS: {:.2})", fps));
+            fps_counter = 0;
+            fps_timer = Instant::now();
+        }
+
+        // Move the pixel when the arrow keys are pressed
+        if window.is_key_down(Key::Left) && x > 0 {
+            x -= 1;
+        }
+        if window.is_key_down(Key::Right) && x < WIDTH - 1 {
+            x += 1;
+        }
+        if window.is_key_down(Key::Up) && y > 0 {
+            y -= 1;
+        }
+        if window.is_key_down(Key::Down) && y < HEIGHT - 1 {
+            y += 1;
+        }
+
+        // Set the pixel to blue
+        buffer[y * WIDTH + x] = 0x0000FFFF;
+
+        // Update the window buffer and display the changes
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
+}
